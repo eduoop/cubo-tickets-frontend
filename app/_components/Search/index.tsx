c85e69abecab8817df1289cd45bf6c11eddb1daf
Eduardo <eduardo.meneses@c2sglobal.com>
2024-10-05 22:54:01 -0300

    feat: render search component only on homepage

--- app/_components/Search/index.tsx
+++ app/_components/Search/index.tsx
@@ -2,11 +2,17 @@
 
 import { Input } from "@/app/_components/ui/input";
 import { useSearch } from "@/app/_contexts/SearchContext";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { LuSearch } from "react-icons/lu";
 
 function Search() {
   const { setSearchTerm, searchTerm } = useSearch();
+  const pathname = usePathname();
+
+  if (pathname !== "/") {
+    return null;
+  }
 
   return (
     <div className="relative min-w-[100px] md:min-w-[500px] py-4 group">
@@ -20,7 +26,7 @@ function Search() {
       <Input
         onChange={(e) => setSearchTerm(e.target.value)}
         value={searchTerm}
-        placeholder="Pesquisar ticket ou menssagem"
+        placeholder="Pesquisar ticket ou mensagem"
         className="w-full rounded-[24px] py-5 pl-12 text-[15px] font-medium border border-solid border-gray-500"
       />
     </div>
